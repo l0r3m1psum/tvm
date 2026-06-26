0d1820b39568e8f702eac0b97dce3055b0cde5b3
Diego Bellani <bellanidiego@gmail.com>
2026-06-26 18:45:59 +0200

    Added initial implementation of LiteRT/TFLite semantics

--- python/tvm/relax/transform/qnn_transforms.py
+++ python/tvm/relax/transform/qnn_transforms.py
@@ -1,7 +1,51 @@
 import tvm_ffi
 from tvm import ir, relax
-import numpy
+import numpy.typing
 import warnings
+from typing import List, Tuple, Literal
+from tvm import topi
+
+# https://github.com/google-ai-edge/LiteRT/blob/a8de8d054d684dfa917d4dd4351b9126a767e38b/tflite/kernels/internal/quantization_util.cc#L53
+def compute_fixed_point_multiplier_and_shift(
+    double_multipliers: numpy.typing.ArrayLike,
+    rounding: Literal["single", "double"] = "single"
+) -> Tuple[numpy.typing.ArrayLike, numpy.typing.ArrayLike]:
+    double_multipliers = numpy.asarray(double_multipliers)
+    if double_multipliers.dtype != numpy.float64: raise ValueError("Input must be a float64")
+
+    quantized_multipliers = numpy.zeros_like(double_multipliers, dtype=numpy.int32)
+    shifts = numpy.zeros_like(double_multipliers, dtype=numpy.int32)
+
+    mask = (double_multipliers != 0.0)
+
+    active_multipliers = double_multipliers[mask]
+
+    q, shift = numpy.frexp(active_multipliers)
+
+    assert ((0.5 <= q) & (q < 1.0)).all()
+
+    q_fixed = numpy.round(q * (1 << 31)).astype(numpy.int64)
+
+    adjust_mask = (q_fixed == (1 << 31))
+    q_fixed[adjust_mask] //= 2
+    shift[adjust_mask] += 1
+
+    zero_mask = shift < -31
+    q_fixed[zero_mask] = 0
+    shift[zero_mask] = 0
+
+    if rounding == "single":
+        saturate_mask = shift > 30
+        q_fixed[saturate_mask] = (1 << 31) - 1
+        shift[saturate_mask] = 30
+
+    quantized_multipliers[mask] = q_fixed.astype(numpy.int32)
+    shifts[mask] = shift.astype(numpy.int32)
+
+    if quantized_multipliers.size == 1:
+        return quantized_multipliers.item(), shift.item()
+    else:
+        return quantized_multipliers, shifts
 
 @ir.transform.module_pass(opt_level=0)
 class NormalizeQDQPatterns:
@@ -320,9 +364,6 @@ class RewriteQDQPatternsToQNNOps:
 
         return mod
 
-from typing import List
-from tvm import topi
-
 def to_int_list(xs: tvm_ffi.Array) -> List[int]:
     return [int(x) for x in xs]
 
@@ -354,9 +395,88 @@ def requantize(s: relax.Constant, x: relax.Expr, z: relax.Constant) -> relax.Exp
     res = clamp(res, -128., 127.).astype("int8")
     return res
 
-# TODO: this should accept a parameter for simulated quantization...
+# MultiplyByQuantizedMultiplier with DOUBLE_ROUND semantics is implemented using
+# two functions from gemmlowp SaturatingRoundingDoublingHighMul and RoundingDivideByPOT
+# https://github.com/google-ai-edge/LiteRT/blob/2465f6422ec9e922699270d955f75200eecefee4/tflite/kernels/internal/common.cc#L67
+# https://github.com/google/gemmlowp/blob/16e8662c34917be0065110bfcd9cc27d30f52fdf/fixedpoint/fixedpoint.h#L340
+# https://github.com/google/gemmlowp/blob/16e8662c34917be0065110bfcd9cc27d30f52fdf/fixedpoint/fixedpoint.h#L368
+
+# This is implemented with single round semantics
+# https://github.com/google-ai-edge/LiteRT/blob/2465f6422ec9e922699270d955f75200eecefee4/tflite/kernels/internal/common.cc#L22
+def multiply_by_quantized_multiplier(
+    x: relax.Expr,
+    quantized_multiplier: numpy.typing.ArrayLike,
+    shift: numpy.typing.ArrayLike
+) -> relax.Expr:
+    """Fixed-point Q0.31 multiplication"""
+    quantized_multiplier = numpy.asarray(quantized_multiplier)
+    shift = numpy.asarray(shift)
+
+    assert (quantized_multiplier >= 0).all()
+    assert ((shift >= -31) & (shift <= 30)).all()
+
+    x_64 = x.astype("int64")
+    m_64 = relax.const(quantized_multiplier, dtype="int64")
+
+    total_shift = 31 - shift.astype("int64")
+
+    round_val = 1 << (total_shift - 1)
+
+    result = (x_64 * m_64) + relax.const(round_val, dtype="int64")
+    result = relax.op.right_shift(result, relax.const(total_shift, dtype="int64"))
+
+    return result.astype("int32")
+
+def requantize_litert(s: relax.Constant, x: relax.Expr, z: relax.Constant) -> relax.Expr:
+    res = x
+    if not (s.data.numpy() == 1).all():
+        m, shift = compute_fixed_point_multiplier_and_shift(s.data.numpy())
+        res = multiply_by_quantized_multiplier(res, m, shift)
+    if not (z.data.numpy() == 0).all():
+        res = res + z.astype("int32")
+    res = clamp(res, -128, 127).astype("int8")
+    return res
+
+# ONNXRuntime does all the scale calculations in 32 bit floating point
+# https://github.com/microsoft/onnxruntime/blob/a203dfafc94b5446a59ddd67e92e6b4b66b01d7e/onnxruntime/core/providers/cpu/quantization/quantize_linear_matmul.cc#L114-L122
+# https://github.com/microsoft/onnxruntime/blob/a203dfafc94b5446a59ddd67e92e6b4b66b01d7e/onnxruntime/core/providers/cpu/quantization/qlinearconv.cc#L87-L96
+# TFLite instead has the crazy single rounding vs. double rounding situation
+# https://github.com/google-ai-edge/LiteRT/blob/a8de8d054d684dfa917d4dd4351b9126a767e38b/tflite/kernels/internal/reference/integer_ops/fully_connected.h#L171-172
+# https://github.com/google-ai-edge/LiteRT/blob/a8de8d054d684dfa917d4dd4351b9126a767e38b/tflite/kernels/internal/reference/integer_ops/conv.h#L125-L126
+# which effects both how the quantized multiplier is applied and how it is calculated
+# https://github.com/google-ai-edge/LiteRT/blob/3c1752349c6f2b73e257989d048b1e7738df9722/tflite/converter/kernels/internal/quantization_util.cc#L55
+# https://github.com/google-ai-edge/LiteRT/blob/3c1752349c6f2b73e257989d048b1e7738df9722/tflite/converter/kernels/internal/common.cc#L23
+# in both cases the quantized multiplier is calculated from scales promoted to
+# 64 bit floating point numbers.
+# For some inexplicable reason the reference implementation of the fully
+# connected layer in LiteRT is does not uses integer-arithmetic-only but uses
+# floating point scales
+# https://github.com/google-ai-edge/LiteRT/blob/3c1752349c6f2b73e257989d048b1e7738df9722/tflite/kernels/fully_connected.cc#L1308-L1313
+# but the usual delegate used for optimized execution XNNPACK implements the
+# fully connected layer with single rounding (in this context is called RNDNU
+# i.e. Round to Nearest Tie to Up)
+# https://github.com/google/XNNPACK/blob/85ba6247ad2168015ab083eaca1d72279b6f8c39/src/qs8-gemm/scalar.c.in#L471-L490
+# XNNPACK also calculates the quantized multiplier hence there is not the
+# possibility for LiteRT to use parameters meant for double rounding to a single
+# rounding kernel (even though it does not make a big difference most of the times)
+# https://github.com/google/XNNPACK/blob/85ba6247ad2168015ab083eaca1d72279b6f8c39/src/microparams-init.c#L92
+# In general calculating the quantized multiplier is a responsibility of the
+# delegate.
+# https://github.com/google-ai-edge/LiteRT/blob/bb8271da6e6f119da9c77aff8a509e83e6df4da7/tflite/delegates/xnnpack/xnnpack_delegate.cc#L4850
+# The more mathematically precise and performant implementation should be the
+# single rounding one, but even when using the XNNPACK delegate it does not
+# implement all TFLite operations and the fallback that LiteRT uses gemmlowp
+# which uses double rounding. Hence networks are executed with operations that
+# have different rounding modes...
+# https://github.com/google-ai-edge/LiteRT/issues/7441
 @relax.expr_functor.mutator
 class LowerQNNOpsMutator(relax.PyExprMutator):
+
+    # TODO: this should accept a parameter for simulated quantization...
+    def __init__(self, mod: ir.IRModule, semantic: Literal["onnx", "litert"]) -> None:
+        super().__init__(mod)
+        self.semantic = semantic
+
     def visit_call_(self, call: relax.Call) -> relax.Call:
         res = call
         op_name = getattr(call.op, "name", "")
@@ -384,17 +504,6 @@ class LowerQNNOpsMutator(relax.PyExprMutator):
                 )
             ):
                 raise ValueError("Per-block quantization is not supported yet")
-
-            # The result should be correctly rounded.
-            m = relax.const(
-                (x_s.data.numpy().astype("float64")
-                    * w_s.data.numpy().astype("float64"))
-                / y_s.data.numpy().astype("float64"),
-                dtype="float32"
-            )
-            assert m.data.numpy().size == max(
-                x_s.data.numpy().size, w_s.data.numpy().size, y_s.data.numpy().size
-            ), "An unexpected broadcasting happened e.g. (C_out, 1, 1, 1) * (1, C_out, 1, 1) = (C_out, C_out, 1, 1)"
 
             x_ones = relax.op.ones_like(x)
             w_ones = relax.op.ones_like(w)
@@ -432,7 +541,31 @@ class LowerQNNOpsMutator(relax.PyExprMutator):
             if b:
                 res += b
 
-            res = requantize(m, res.astype("float32"), y_zp)
+            if self.semantic == "onnx":
+                m = relax.const(
+                    (x_s.data.numpy() * w_s.data.numpy()) / y_s.data.numpy(),
+                    dtype="float32"
+                )
+            elif self.semantic == "litert":
+                m = relax.const(
+                    (x_s.data.numpy().astype("float64") * w_s.data.numpy()) / y_s.data.numpy(),
+                    dtype="float64"
+                )
+            else:
+                assert False
+
+            assert m.data.numpy().size == max(
+                x_s.data.numpy().size, w_s.data.numpy().size, y_s.data.numpy().size
+            ), "An unexpected broadcasting happened e.g. (C_out, 1, 1, 1) * (1, C_out, 1, 1) = (C_out, C_out, 1, 1)"
+
+            if self.semantic == "onnx":
+                res = requantize(m, res.astype("float32"), y_zp)
+            elif self.semantic == "litert":
+                # https://github.com/google-ai-edge/LiteRT/blob/a8de8d054d684dfa917d4dd4351b9126a767e38b/tflite/kernels/internal/reference/integer_ops/conv.h#L26
+                # https://github.com/google-ai-edge/LiteRT/blob/a8de8d054d684dfa917d4dd4351b9126a767e38b/tflite/kernels/internal/reference/integer_ops/fully_connected.h#L136
+                res = requantize_litert(m, res, y_zp)
+            else:
+                assert False
         elif op_name == "relax.qnn.add":
             (
                 a, a_s, a_zp,
@@ -459,28 +592,62 @@ class LowerQNNOpsMutator(relax.PyExprMutator):
 
             # C = (A_s * (A - A_z) + B_s * (B - B_z))/C_s + C_z
             # C = A_s/C_s * (A - A_z) + B_s/C_s * (B - B_z) + C_z
-            c_s_float = c_s.data.numpy()
+            if self.semantic == "onnx":
+                c_s_float = c_s.data.numpy()
+            elif self.semantic == "litert":
+                c_s_float = c_s.data.numpy().astype(numpy.float64)
+            else:
+                assert False
+
             a_m_float = a_s.data.numpy() / c_s_float
             b_m_float = b_s.data.numpy() / c_s_float
-            a_m = relax.const(a_m_float)
-            b_m = relax.const(b_m_float)
+
+            if self.semantic == "onnx":
+                a_m = relax.const(a_m_float)
+                b_m = relax.const(b_m_float)
+            elif self.semantic == "litert":
+                a_m = relax.const(a_m_float, dtype="float64")
+                b_m = relax.const(b_m_float, dtype="float64")
+            else:
+                assert False
+
+            # FIXME: for addition it should do a different thing when self.semantic == "litert".
 
             lhs = a.astype("int32")
             if not (a_zp.data.numpy() == 0).all():
                 lhs -= const_astype(a_zp, "int32")
-            lhs = lhs.astype("float32")
-            if not (a_m_float == 1).all():
-                lhs *= a_m
+            if self.semantic == "onnx":
+                lhs = lhs.astype("float32")
+                if not (a_m_float == 1).all():
+                    lhs *= a_m
+            elif self.semantic == "litert":
+                if not (a_m_float == 1).all():
+                    m, s = compute_fixed_point_multiplier_and_shift(a_m_float)
+                    lhs = multiply_by_quantized_multiplier(lhs, m, s)
+            else:
+                assert False
 
             rhs = b.astype("int32")
             if not (b_zp.data.numpy() == 0).all():
                 rhs -= const_astype(b_zp, "int32")
-            rhs = rhs.astype("float32")
-            if not (b_m_float == 1).all():
-                rhs *= b_m
+            if self.semantic == "onnx":
+                rhs = rhs.astype("float32")
+                if not (b_m_float == 1).all():
+                    rhs *= b_m
+            elif self.semantic == "litert":
+                if not (b_m_float == 1).all():
+                    m, s = compute_fixed_point_multiplier_and_shift(b_m_float)
+                    rhs = multiply_by_quantized_multiplier(rhs, m, s)
+            else:
+                assert False
 
             res = lhs + rhs
-            res = requantize(relax.const(1.0), res, c_zp)
+            if self.semantic == "onnx":
+                res = requantize(relax.const(1.0), res, c_zp)
+            elif self.semantic == "litert":
+                res = requantize_litert(relax.const(1.0, dtype="float64"), res, c_zp)
+            else:
+                assert False
         elif op_name == "relax.qnn.avg_pool2d":
             (
                 x, x_s, x_zp,
@@ -506,7 +673,6 @@ class LowerQNNOpsMutator(relax.PyExprMutator):
             # sum followed by a fixed-point multiplication.
             # NOTE: can be implemented as a convolution with a kernel of ones
             # followed by a division by N (which can be fused in the multiplier)
-            m = relax.const(x_s.data.numpy()/y_s.data.numpy())
             res = relax.op.nn.avg_pool2d(
                 data=x.astype("int32"),
                 **relax.op.qnn.avg_pool2d_attrs_to_dict(call.attrs)
@@ -514,8 +680,19 @@ class LowerQNNOpsMutator(relax.PyExprMutator):
             if (x_zp.data.numpy() != 0).any():
                 res -= const_astype(x_zp, "int32")
 
-            res = requantize(m, res.astype("float32"), y_zp)
+            if self.semantic == "onnx":
+                m = relax.const(x_s.data.numpy()/y_s.data.numpy())
+            elif self.semantic == "litert":
+                m = relax.const(x_s.data.numpy().astype("float64")/y_s.data.numpy(), dtype="float64")
+            else:
+                assert False
 
+            if self.semantic == "onnx":
+                res = requantize(m, res.astype("float32"), y_zp)
+            elif self.semantic == "litert":
+                res = requantize_litert(m, res, y_zp)
+            else:
+                assert False
         if res is call:
             res = super().visit_call_(call)
 
@@ -523,8 +700,16 @@ class LowerQNNOpsMutator(relax.PyExprMutator):
 
 @ir.transform.module_pass(opt_level=0)
 class LowerQNNOps:
+    """Lowers quantized operators to a sequence of operations that implement the
+    ONNX semantic of quantized operators (i.e. round-to-nearest with
+    scale at the end) or LiteRT semantic (i.e. integer-arithmetic-only)."""
+
+    def __init__(self, semantic: Literal["onnx", "litert"]) -> None:
+        super().__init__()
+        self.semantic = semantic
+
     def transform_module(self, mod: ir.IRModule, _ctx: ir.transform.PassContext) -> ir.IRModule:
-        mutator = LowerQNNOpsMutator(mod)
+        mutator = LowerQNNOpsMutator(mod, self.semantic)
 
         for global_var, func in mod.functions.items():
             if isinstance(func, relax.Function):
