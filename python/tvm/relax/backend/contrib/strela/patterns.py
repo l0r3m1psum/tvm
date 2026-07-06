a5381baaf695ce52ecc4ca8e0adbe553c81e296b
Diego Bellani <bellanidiego@gmail.com>
2026-07-06 12:43:54 +0200

    Rewritten pattern

--- python/tvm/relax/backend/contrib/strela/patterns.py
+++ python/tvm/relax/backend/contrib/strela/patterns.py
@@ -3,9 +3,12 @@ from tvm.relax.transform import PatternCheckContext, FusionPattern
 
 from ...pattern_registry import register_patterns
 
+def _check_always_ok(context: PatternCheckContext) -> bool:
+    return True
+
 def relu_pattern() -> FusionPattern:
 
-    input_tensor = wildcard()
+    input_tensor = wildcard().has_dtype("int32")
     relu = is_op("relax.nn.relu")(input_tensor)
 
     annotations = {
@@ -13,43 +16,60 @@ def relu_pattern() -> FusionPattern:
         "root": relu,
     }
 
-    def _check_relu(context: PatternCheckContext) -> bool:
-        return True
+    return FusionPattern("strela.relu", relu, annotations, _check_always_ok)
 
-    return FusionPattern("strela.relu", relu, annotations, _check_relu)
+def centered_biliear_product_pattern() -> FusionPattern:
+    """This pattern is more robust than the non factorized version e.g. for
+    matrix multiplication
 
-def qnn_linear_pattern() -> FusionPattern:
-    """This pattern is fragile since it is susceptible to both commutativity and
-    associativity of the operators involved. Not only that but if the patter is
-    written with matrix multiplications and matrices of ones, instead of sums
-    again it would not be recognized. To solve this kind of problems an approach
-    like equality saturation would be needed.
+        (W-z_w) (x-z_x) = W x - z_x sum(W) - z_w sum(x) + n z_x z_w
 
-    "Latent Idiom Recognition for a Minimalist Functional Array Language using
-    Equality Saturation"
+    which is susceptible to both commutativity and associativity of the
+    operators involved. Notice also that instead of using sum in the above
+    equation we could have used matrix multiplication and matrices of ones.
+    Solving this idiom recognition in the general case requires an equality
+    saturation approach [0].
+
+    This pattern is still not invariant to rewriting subtraction as addition but
+    given that the LHS and RHS are the same it is not susceptible to
+    commutativity. The problem can be solving by canonicalizing subtraction by
+    a constant as addition with that negated constant, like LLVM does [1], but
+    TVM doesn't do this.
+
+    [0]: "Latent Idiom Recognition for a Minimalist Functional Array Language using Equality Saturation"
+    [1]: https://github.com/llvm/llvm-project/blob/4ffcfdfac177a7e7b3d9be20f73dd9d2890da006/llvm/lib/Transforms/InstCombine/InstCombineAddSub.cpp#L2451
     """
 
-    weight = wildcard().has_dtype("int8") # Not is_const because of permutation
+    # This is not a constant because operations like permutation.
+    weight = wildcard().has_dtype("int8")
     input = wildcard().has_dtype("int8")
-    weight_zp = is_const().has_dtype("int32").has_shape(())
-    input_zp = is_const().has_dtype("int32").has_shape(())
-    n = is_const().has_dtype("int32")
+    weight_zp = is_const().has_dtype("int8")
+    # Lifting the constant requirement here could allow for dynamic quantization.
+    input_zp = is_const().has_dtype("int8")
 
-    # W x - z_x sum(W) - z_w sum(x) + n z_x z_w
-    weight_input = is_op("relax.matmul")(input, weight).has_dtype("int32")
-    input_zp_weight = is_op("relax.multiply")(
-        input_zp, is_op("relax.sum")(is_op("relax.astype")(weight))
+    centered_input = is_op("relax.subtract")(
+        is_op("relax.astype")(input).has_dtype("int32"),
+        is_op("relax.astype")(input_zp).has_dtype("int32"),
     )
-    weight_zp_input = is_op("relax.multiply")(
-        weight_zp, is_op("relax.sum")(is_op("relax.astype")(input))
+    centered_weight = is_op("relax.subtract")(
+        is_op("relax.astype")(weight).has_dtype("int32"),
+        is_op("relax.astype")(weight_zp).has_dtype("int32"),
     )
-    input_zp_weight_zp = is_op("relax.multiply")(
-        n, is_op("relax.multiply")(input_zp, weight_zp)
+    pattern = (
+        is_op("relax.matmul") | is_op("relax.nn.conv2d") | is_op("relax.multiply")
+    )(
+        centered_input, centered_weight
     )
-    pattern = weight_input
-    pattern = pattern | is_op("relax.subtract")(weight_input, input_zp_weight)
-    pattern = pattern | is_op("relax.subtract")(pattern, weight_zp_input)
-    pattern = pattern | is_op("relax.add")(pattern, input_zp_weight_zp)
+
+    # The name of the fused operation is the topological order of the operations
+    # plus the order of the arguments of the various operations (starting from
+    # the root). For this pattern it is
+    # fused
+    #     _relax_astype_relax_astype_relax_subtract
+    #     _relax_astype_relax_astype_relax_subtract
+    #     _relax_(matmul|nn_conv2d|multiply)
+    # Notice that the first "astype astype subtract" is the one of the input
+    # and the second one is the one of the weight.
 
     annotations = {
         "weight": weight,
@@ -57,34 +77,17 @@ def qnn_linear_pattern() -> FusionPattern:
         "weight_zp": weight_zp,
         "input_zp": input_zp,
 
-        "weight_input": weight_input,
-        "input_zp_weight": input_zp_weight,
-        "weight_zp_input": weight_zp_input,
-        "input_zp_weight_zp": input_zp_weight_zp,
+        "root": pattern,
     }
 
-    def _check_qnn_linear(context: PatternCheckContext) -> bool:
-        # Check that the pattern is realizable by expanding (W-z_w)(x-z_x)
-        # (W-z_w) (x-z_x) = W x - z_x sum(W) - z_w sum(x) + n z_x z_w
-        # W (x-z_x)       = W x - z_x sum(W)
-        # (W-z_w) x       = W x - z_w sum(x)
-        # W x             = W x
-        # W x must be present, n z_x z_w is present iff both z_x sum(W) and
-        # z_w sum(x) are.
-        map = context.annotated_expr
-        res = (
-            "weight_input" in map
-            and (("input_zp_weight_zp" in map)
-                == ("input_zp_weight" in map and "weight_zp_input" in map)
-            )
-        )
-        return res
+    return FusionPattern(
+        "strela.centered_biliear_product", pattern, annotations, _check_always_ok
+    )
 
-    return FusionPattern("strela.qnn_linear", pattern, annotations, _check_qnn_linear)
 
 register_patterns(
     [
         relu_pattern(),
-        qnn_linear_pattern(),
+        centered_biliear_product_pattern(),
     ]
 )
